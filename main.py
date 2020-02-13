7e8093431211191ffd56257a9c2e835c9ed181e1
Ruth Whelan-Jeans MPHYS 2020 <ruthwj@slioch.jb.man.ac.uk>
2020-02-13 16:53:02 +0000

    plotting added

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import matplotlib.pyplot as plt
 import sys
 from astropy.io import ascii
 
-def generate_pulsars(): 
-    """"""
-def read_pulsars():
+#def generate_pulsars(): 
 
-def bi_test():
+#def read_pulsars():
 
-def plot_pulsars(): 
+#def bi_test():
 
-def constrain_parameter(): #will likely extend this to individual functions for each paramter or a class
+#def plot_pulsars(): 
+
+#def constrain_parameter(): #will likely extend this to individual functions for each paramter or a class
 
 #test
 
 data = ascii.read("weak.all37.p3fold.ASCII", data_start=1)
 print(data)
+df = data.to_pandas()
+print(df)
+
+plt.hist2d(df.col4, df.col3, bins=(2245, 2245), cmap=plt.cm.jet)
+plt.show()
+
+ 
+
