4696906d78dedb93ea12247adfe3a64d6176edd9
George Searle <gpwhs@users.noreply.github.com>
2020-03-03 16:04:23 +0000

    dict

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ ref_pulsar = subprocess.Popen(pulsar_arg)
 arg_names = ["n1", "p4_1", "phi0_1", "fP4_1", "t4_1", "phi4_1", "e1", "or1", "s2", "I2", "rho2", "w2", "n2", "p4_2", "phi0_2", "fP4_2", "t4_2", "phi4_2", "e2", "or2", "s2", "a", "b"] 
 
 def generate_pulsars(inc):
-	global pulsars{} 
+	global pulsars = {} 
 	for j in range(13) and i in [x for x in range(27) if x != 0]:
 		pulsar_arg[i]+=inc
 		pulsar_arg[28]="SimPulse{}{}.gg".format(j, inc)
