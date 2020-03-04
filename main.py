89a275de25447815972fb32fab5a88e4b8609519
gpwhs <george.searle34@gmail.com>
2020-03-04 23:07:09 +0000

    testing pipeline

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ pulsars = {}
 def generate_pulsars(inc):
 	for j in range(13):
 		for i in [x for x in range(27) if x != 0]:
-			pulsar_arg[i]=(float(pulsar_arg[i])+inc).tostr()
-			pulsar_arg[28]="SimPulse{}{}.gg".format(j.tostr(), inc.tostr())
+			pulsar_arg[i]=string((float(pulsar_arg[i])+inc))
+			pulsar_arg[28]="SimPulse{}{}.gg".format(string(j), string(inc))
 			pulsars["Pulsar{}{}".format(j, arg_names[i-1])] = subprocess.check_output(pulsar_arg, shell=True)
 
 
