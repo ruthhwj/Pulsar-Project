7d2be729ae137f3a5b94f9b4232eb3f7daf8fc67
gpwhs <george.searle34@gmail.com>
2020-05-08 22:43:03 +0100

    this stuff actually works

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from astropy.io import ascii
 import numpy as np
 import csv
 #should be working
-pulsar=["./pulsar-getter.sh", "233.940149", "10.5", "1", "15" , "17", "0.85", "45", "60", "7.7", "1", "15", "29", "refpulsar.gg"]
+pulsar=["./pulsar-getter.sh", "250", "10.5", "1", "15" , "17", "0.8", "45", "80", "7.7", "0.85", "15", "29", "refpulsar.gg"]
 pulsar_arg_names = ["scriptname", "Cone1Intensity", "Cone1BeamAngle", "Cone1BeamletAngle","Cone1NumberOfSparks", "Cone1phi0", "Eccentricity", "Orientation", "Cone2Intensity",
                     "Cone2BeamAngle", "Cone2BeamletAngle","Cone2NumberOfSparks", "Cone2phi0", "Filename"]
-pulsar_arg_ranges = [[230, 260], [8, 12], [1, 2], [15, 15], [10,20] , [0.5, 0.9], [40, 50], [40, 100], [6,10], [0.5,1.5], [15,15], [22,32]] #ranges over which to search for each variable
-
+pulsar_arg_ranges = [[230, 260], [8, 12], [1, 2], [15, 15], [10,20] , [0.5, 0.9], [40, 50], [60, 120], [6,10], [0.5,1.5], [15,15], [22,32]] #ranges over which to search for each variable
 def read_pulsar(string): # Reads ASCII, returns dataframe  #"weak.all37.p3fold.ASCII"
   data = ascii.read(string, data_start=1)
   df = data.to_pandas()
@@ -72,7 +71,7 @@ def compare_pulsars_all(pulsar_number, N, intensities_exp_flat):
 
 def pulsar_worker_1d(arg, exp): # int argument,
     n = 1
-    N=5000
+    N=500
     res = []
     while n<=N:
         pulsar_number=str(n)
