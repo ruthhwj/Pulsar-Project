5de7c26250a49acf8335ba029c6c14f27a022293
Ruth <56967050+ruthhwj@users.noreply.github.com>
2020-06-16 11:19:56 +0100

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,15 +1,6 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import subprocess
-import string
 from astropy.io import ascii
-import glob, os
-import sklearn
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.cm as cmx
-import scipy as sp
-from mpl_toolkits import mplot3d
 import numpy as np
 
 pulsar=["./pulsar-getter.sh", "233.940149", "10.5", "1", "15" , "17", "0.85", "45", "60", "7.7", "1", "15", "29", "refpulsar.gg"]
@@ -51,11 +42,21 @@ def get_intensities(df, flag):  # Reads dataframe, returns 50x2246 array for plo
 def plot_pulsar(df_pixelarray):
  print("Plotting pulsar...")
  plt.imshow(df_pixelarray, 'afmhot', origin='lower', interpolation='none', aspect='auto',extent=[700,1000,0,50])
- plt.colorbar()
+ plt.xlabel("Pulse bin")
+ plt.ylabel('Pulse Subint')
+ #plt.colorbar()
  plt.show()
 
 def gaussian(x, mu, sig):
   return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+
+def fit_measure(intensities_ref, intensities_img):
+    chi = 0
+    for i in range(len(intensities_ref)):
+        x1 = (intensities_img[i] - intensities_ref[i])
+        chi += abs(x1 * x1)/ (RMS_noise * (50 * 300 - 2))
+    return chi   #
+
 
 def brighten(exp_data):
   i = 0
@@ -80,16 +81,82 @@ pd.set_option('display.width', 1000)
 
 #data preprocessing
 
-#df_exp = read_pulsar("weak.all37.p3fold.rebinned.ASCII")  # experimental p3fold here
-#intensities_exp = brighten(get_intensities(df_exp, 0)).flatten()
 
-for i in [x for x in range(1,13) if (x!=4 and x!=11 and x!=1 and x!=8)]:
-  data1d = pd.read_csv(r'Results\1k 1D\results{}.txt'.format(pulsar_arg_names[i]), sep=",", header=None)
-  data1d.columns = ["col1", "chi"]
-  print(data1d.nsmallest(5, 'chi'))
-  plt.scatter(data1d.col1, data1d.chi, linewidth=1)
-  plt.xlabel(pulsar_arg_names[i])
+
+df_exp = read_pulsar("weak.all37.p3fold.rebinned.ASCII")  # experimental p3fold here
+intensities_exp = brighten(get_intensities(df_exp, 0)).flatten()
+intensities_RMS = np.array(df_exp.col4)
+exp_croppedlist = ((intensities_RMS.reshape(50, 1123))[:, 0:600]).flatten()  # off pulse RMS noise
+RMS_noise = np.var(exp_croppedlist)
+print(RMS_noise)
+
+
+#attempt to get individual chis out for chosen ASCII files
+
+"""
+df_ref = read_pulsar("realreferencepulsar.ASCII")
+intensities_ref = get_intensities(df_exp,1)
+fitmeasure = fit_measure(intensities_ref, intensities_exp)
+print(fitmeasure)
+plot_pulsar(get_intensities(df_ref,0))
+"""
+
+
+
+
+# 1D plots
+N=[100,500,1000]
+
+for j in N:
+  for i in [x for x in range(1,13) if (x!=4 and x!=11)]:
+    data1d = pd.read_csv(r'Results\N = {} 1D\results{}.txt'.format(j,pulsar_arg_names[i]), sep=",", header=None)
+    data1d.columns = ["col1", "chi"]
+    print(data1d.nsmallest(5, 'chi'))
+    plt.scatter(data1d.col1, data1d.chi, linewidth=1)
+    plt.xlabel(pulsar_arg_names[i])
+    plt.ylabel('Reduced Chi Squared')
+    plt.title("N="+str(j))
+    plt.savefig('1D_graphs\AllResults_N{}_{}.png'.format(j, pulsar_arg_names[i]))
+    plt.show()
+
+"""
+
+# ND plots
+
+N = 10000  #change this depending on which dataset you want to look at
+col = []
+dataND = pd.read_csv('AllResults_N{}.csv'.format(N), sep=",", header=None) #has 11 columns
+
+for i in [x for x in range(1,13) if (x!=4 and x!=11)]:
+  col.append(pulsar_arg_names[i])
+col.append("chi")
+print(col)
+dataND.columns = col
+print(dataND.nsmallest(20, 'chi')) # outputs args of the 20 minimum chi pulsars
+for column in dataND:
+  contents = dataND[column]
+  plt.scatter(contents, dataND.chi, linewidth=1)
+  plt.xlabel(column)
   plt.ylabel('Reduced Chi Squared')
-  plt.title("N=1000")
+  plt.title("N="+str(N))
+  plt.savefig('NDResults\AllResults_N{}_{}.png'.format(N, column))
   plt.show()
+  
+  
+# Plot minimum points of a data set against simulation number
 
+array = np.array(dataND.chi)
+min_chi_results = []
+min_chi =10000
+for j in range(len(array)):
+  if array[j] < min_chi:
+    min_chi = array[j]
+    min_chi_results.append([int(j), min_chi])
+    
+min_chi_results = np.array(min_chi_results)
+plt.scatter(min_chi_results[:,0],min_chi_results[:,1], linewidth=1)
+plt.xlabel("Simulation number")
+plt.ylabel("New minimum reduced Chi squared")
+plt.show()
+
+"""
